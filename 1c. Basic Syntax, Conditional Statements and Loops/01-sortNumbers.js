7ba3e6b477729f07de0d20e98b26074e9178930d
Atanas Bondzhakov <156101290+AtanasBondzhakov@users.noreply.github.com>
2024-05-24 22:03:17 +0300

    Update 01-sortNumbers.js

--- 1c. Basic Syntax, Conditional Statements and Loops/01-sortNumbers.js
+++ 1c. Basic Syntax, Conditional Statements and Loops/01-sortNumbers.js
@@ -36,3 +36,7 @@ function sortNumbers(numA, numB, numC) {
     console.log(mid);
     console.log(min);
 }
+
+sortNumbers(2, 1, 3); // 3 2 1
+sortNumbers(-2, 1, 3); // 3 1 -2
+sortNumbers(0, 0, 3); // 2 0 0
