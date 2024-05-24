7a4f80fbb5bfc3859980bf610c227ab0c098385e
Atanas Bondzhakov <156101290+AtanasBondzhakov@users.noreply.github.com>
2024-05-24 07:55:08 +0300

    Add files via upload

---
+++ 2a. Data Types and Variables/09-specialNumbers.js
@@ -0,0 +1,21 @@
+function specialNumbers(num) {
+    for (let i = 1; i <= num; i++) {
+        let numAsStr = i.toString();
+        let digitSum = 0;
+        let isSpecial = 'False';
+
+        for (let j = 0; j < numAsStr.length; j++) {
+            let currentDigit = Number(numAsStr[j]);
+            digitSum += currentDigit;
+        }
+        
+        if (digitSum === 5 || digitSum === 7 || digitSum === 11) {
+            isSpecial = 'True';
+        }
+
+        console.log(`${i} -> ${isSpecial}`);
+    }
+}
+
+specialNumbers(15);
+// specialNumbers(20);
