cfaaef817be882d5f3ea99719a1a3e34d7e1225e
Atanas Bondzhakov <156101290+AtanasBondzhakov@users.noreply.github.com>
2024-05-25 20:22:43 +0300

    Update 01-multiplyNumberBy2.js

--- 1a. Basic Syntax, Conditional Statements and Loops/01-multiplyNumberBy2.js
+++ 1a. Basic Syntax, Conditional Statements and Loops/01-multiplyNumberBy2.js
@@ -2,6 +2,6 @@ function multiplyNumberBy2(num) {
     console.log(num * 2);
 }
 
-multiplyNumberBy2(2);
-multiplyNumberBy2(5);
-multiplyNumberBy2(20);
+multiplyNumberBy2(2); //4
+multiplyNumberBy2(5); //10
+multiplyNumberBy2(20); //40
