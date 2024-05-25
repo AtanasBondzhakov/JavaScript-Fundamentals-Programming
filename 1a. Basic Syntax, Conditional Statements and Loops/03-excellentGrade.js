871bd600a1e8659f0aa372da928d5adbdeefec77
Atanas Bondzhakov <156101290+AtanasBondzhakov@users.noreply.github.com>
2024-05-25 20:23:30 +0300

    Update 03-excellentGrade.js

--- 1a. Basic Syntax, Conditional Statements and Loops/03-excellentGrade.js
+++ 1a. Basic Syntax, Conditional Statements and Loops/03-excellentGrade.js
@@ -2,5 +2,5 @@ function excellentGrade(grade) {
     grade >= 5.50 ? console.log('Excellent') : console.log('Not excellent');
 }
 
-excellentGrade(5.50);
-excellentGrade(4.35);
+excellentGrade(5.50); //Excellent
+excellentGrade(4.35); //Not excellent
