8587f16e40637f28d133788ca20ab97eedeb6ab1
Atanas Bondzhakov <156101290+AtanasBondzhakov@users.noreply.github.com>
2024-05-25 20:23:08 +0300

    Update 02-studentInformation.js

--- 1a. Basic Syntax, Conditional Statements and Loops/02-studentInformation.js
+++ 1a. Basic Syntax, Conditional Statements and Loops/02-studentInformation.js
@@ -2,6 +2,6 @@ function studentInformation(name, age, avgGrade) {
     console.log(`Name: ${name}, Age: ${age}, Grade: ${avgGrade.toFixed(2)}`);
 }
 
-studentInformation('John', 15, 5.54678);
-studentInformation('Steve', 16, 2.1426);
-studentInformation('Marry', 12, 6.00);
+studentInformation('John', 15, 5.54678); //Name: John, Age: 15, Grade: 5.55
+studentInformation('Steve', 16, 2.1426); //Name: Steve, Age: 16, Grade: 2.14
+studentInformation('Marry', 12, 6.00); //Name: Marry, Age: 12, Grade: 6.00
