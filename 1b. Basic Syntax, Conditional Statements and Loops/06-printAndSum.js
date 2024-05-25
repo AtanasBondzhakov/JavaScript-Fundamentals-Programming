31fc5b98e642880d1c0ce6a60af7162830dbe62c
Atanas Bondzhakov <156101290+AtanasBondzhakov@users.noreply.github.com>
2024-05-25 20:17:59 +0300

    Update 06-printAndSum.js

--- 1b. Basic Syntax, Conditional Statements and Loops/06-printAndSum.js
+++ 1b. Basic Syntax, Conditional Statements and Loops/06-printAndSum.js
@@ -9,6 +9,9 @@ function printAndSum(num1, num2) {
     console.log(`Sum: ${sum}`);
 }
 
-printAndSum(5, 10);
-printAndSum(0, 26);
-printAndSum(50, 60);
+printAndSum(5, 10); //5 6 7 8 9 10
+                    //Sum: 45
+printAndSum(0, 26); //0 1 2 … 26
+                    //Sum: 351
+printAndSum(50, 60); //50 51 52 53 54 55 56 57 58 59 60
+                     //Sum: 605
