b545729bcd087186c6dd1c68de2e2ab9727f11d8
Atanas Bondzhakov <156101290+AtanasBondzhakov@users.noreply.github.com>
2024-06-06 21:32:04 +0300

    Add files via upload

---
+++ 3b. Arrays/10-ladybugs.js
@@ -0,0 +1,56 @@
+function ladybugs(input) {
+    let fieldSize = input[0];
+    let ladybugsIndices = input[1].split(' ').map(Number);
+    let field = [];
+
+    for (let i = 0; i < fieldSize; i++) {
+        if (ladybugsIndices.includes(i)) {
+            field[i] = 1;
+        } else {
+            field[i] = 0;
+        }
+    }
+
+    for (let j = 2; j < input.length; j++) {
+        let command = input[j];
+        let tokens = command.split(' ');
+        let ladybugIndex = Number(tokens[0]);
+        let direction = tokens[1];
+        let flyLength = Number(tokens[2]);
+
+        if (!field[ladybugIndex]) {
+            continue;
+        }
+
+        field[ladybugIndex] = 0;
+
+        if (direction === 'left') {
+            let newIndex = ladybugIndex - flyLength;
+
+            while (field[newIndex] === 1) {
+                newIndex -= flyLength;
+            }
+
+            if (newIndex >= 0) {
+                field[newIndex] = 1;
+            }
+
+        } else {
+            let newIndex = ladybugIndex + flyLength;
+
+            while (field[newIndex] === 1) {
+                newIndex += flyLength;
+            }
+
+            if (newIndex < field.length) {
+                field[newIndex] = 1;
+            }
+        }
+    }
+    console.log(field.join(' '));
+}
+
+ladybugs([3, '0 1',
+    '0 right 1',
+    '2 right 1']
+);
