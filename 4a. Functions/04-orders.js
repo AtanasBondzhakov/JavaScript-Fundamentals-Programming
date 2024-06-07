dd402b3c22f1c7ca58f4ad86c6755a85521db444
Atanas Bondzhakov <156101290+AtanasBondzhakov@users.noreply.github.com>
2024-06-07 23:47:05 +0300

    Add files via upload

---
+++ 4a. Functions/04-orders.js
@@ -0,0 +1,23 @@
+function orders(product, quantity) {
+    let total = 0;
+
+    switch (product) {
+        case 'coffee':
+            total = quantity * 1.5;
+            break;
+        case 'water':
+            total = quantity * 1;
+            break;
+        case 'coke':
+            total = quantity * 1.4;
+            break;
+        case 'snacks':
+            total = quantity * 2;
+            break;
+    }
+
+    console.log(total.toFixed(2));
+}
+
+orders("water", 5); //5.00
+orders("coffee", 2); //3.00
