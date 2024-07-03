ac7b0bf348f31a9dc2841588ae3992369aa8932e
Atanas Bondzhakov <156101290+AtanasBondzhakov@users.noreply.github.com>
2024-07-03 14:06:00 +0300

    Update and rename 1b. Basic Syntax, Conditional Statements and Loops/10-thePyramidOfKingDjoser.js to 1. Basic Syntax, Conditional Statements and Loops/Exercises/10-thePyramidOfKingDjoser.js

--- 1. Basic Syntax, Conditional Statements and Loops/Exercises/10-thePyramidOfKingDjoser.js
+++ 1. Basic Syntax, Conditional Statements and Loops/Exercises/10-thePyramidOfKingDjoser.js
@@ -34,9 +34,3 @@ function thePyramidOfKingDjoser(base, increment) {
     console.log(`Gold required: ${totalGold}`);
     console.log(`Final pyramid height: ${Math.floor(height)}`);
 }
-
-thePyramidOfKingDjoser(11, 1); //Stone required: 165
-                               //Marble required: 112
-                               //Lapis Lazuli required: 8
-                               //Gold required: 1
-                               //Final pyramid height: 6
