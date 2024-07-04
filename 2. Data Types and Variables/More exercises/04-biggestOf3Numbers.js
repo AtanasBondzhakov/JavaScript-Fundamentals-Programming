135a610fd17f5af73982d8c163af1acdcd83ffc9
Atanas Bondzhakov <156101290+AtanasBondzhakov@users.noreply.github.com>
2024-07-04 20:56:04 +0300

    Update and rename 2c. Data Types and Variables/04-biggestOf3Numbers.js to 2. Data Types and Variables/More exercises/04-biggestOf3Numbers.js

--- 2. Data Types and Variables/More exercises/04-biggestOf3Numbers.js
+++ 2. Data Types and Variables/More exercises/04-biggestOf3Numbers.js
@@ -13,8 +13,3 @@ function biggestOf3Numbers(numA, numB, numC) {
 
     console.log(biggestNum);
 }
-
-biggestOf3Numbers(-2, 7, 3); //7
-biggestOf3Numbers(130, 5, 99); //130
-biggestOf3Numbers(43, 43.2, 43.1); //43.2
-biggestOf3Numbers(2, 2, 2); //2
