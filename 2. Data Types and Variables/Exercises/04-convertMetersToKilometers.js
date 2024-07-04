6d456abd4e25733315fd776134a0878a16cf75b3
Atanas Bondzhakov <156101290+AtanasBondzhakov@users.noreply.github.com>
2024-07-04 20:50:32 +0300

    Update and rename 2b. Data Types and Variables/04-convertMetersToKilometers.js to 2. Data Types and Variables/Exercises/04-convertMetersToKilometers.js

--- 2. Data Types and Variables/Exercises/04-convertMetersToKilometers.js
+++ 2. Data Types and Variables/Exercises/04-convertMetersToKilometers.js
@@ -2,6 +2,3 @@ function convertMetersToKilometers(meters) {
     let kilometers = meters / 1000;
     console.log(kilometers.toFixed(2));
 }
-
-convertMetersToKilometers(1852); //1.85
-convertMetersToKilometers(798); //0.80
