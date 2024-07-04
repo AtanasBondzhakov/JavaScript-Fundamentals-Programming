7dd9886c7e3afbd15e2139907d0b6c48c85ec8fa
Atanas Bondzhakov <156101290+AtanasBondzhakov@users.noreply.github.com>
2024-07-04 20:51:21 +0300

    Update and rename 2b. Data Types and Variables/06-reversedChars.js to 2. Data Types and Variables/Exercises/06-reversedChars.js

--- 2. Data Types and Variables/Exercises/06-reversedChars.js
+++ 2. Data Types and Variables/Exercises/06-reversedChars.js
@@ -1,6 +1,3 @@
 function reversedChars(firstChar, secondChar, thirdChar) {
     console.log(`${thirdChar} ${secondChar} ${firstChar}`);
 }
-
-reversedChars('A', 'B', 'C'); //C B A
-reversedChars('1', 'L', '&'); //& L 1
