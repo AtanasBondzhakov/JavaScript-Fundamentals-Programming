f8589c272904c501a851250685d648143814a567
Atanas Bondzhakov <156101290+AtanasBondzhakov@users.noreply.github.com>
2024-07-04 20:59:14 +0300

    Update and rename 2c. Data Types and Variables/07-triangleArea.js to 2. Data Types and Variables/More exercises/07-triangleArea.js

--- 2. Data Types and Variables/More exercises/07-triangleArea.js
+++ 2. Data Types and Variables/More exercises/07-triangleArea.js
@@ -3,7 +3,3 @@ function triangleArea(a, b, c) {
     let area = Math.sqrt(semiPerimeter * (semiPerimeter - a) * (semiPerimeter - b) * (semiPerimeter - c));
     console.log(area);
 }
-
-
-triangleArea(2, 3.5, 4); //3. 4994419197923547
-triangleArea(3, 5.5, 4); //5.854685623498498
