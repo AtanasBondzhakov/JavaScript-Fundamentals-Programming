5506f04d34df7e48953f80280477acdc2dbbd436
Atanas Bondzhakov <156101290+AtanasBondzhakov@users.noreply.github.com>
2024-07-04 20:49:44 +0300

    Update and rename 2b. Data Types and Variables/02-charsToString.js to 2. Data Types and Variables/Exercises/02-charsToString.js

--- 2. Data Types and Variables/Exercises/02-charsToString.js
+++ 2. Data Types and Variables/Exercises/02-charsToString.js
@@ -1,7 +1,3 @@
 function charsToString(firstStr, secondStr, thirdStr) {
     console.log(firstStr + secondStr + thirdStr);
 }
-
-charsToString('a', 'b', 'c'); //abc
-charsToString('%', '2', 'o'); //%2o
-charsToString('1', '5', 'p'); //15p
