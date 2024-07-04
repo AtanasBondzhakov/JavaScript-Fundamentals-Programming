5df1ce60a171391946bdd2b5185701f3779beb78
Atanas Bondzhakov <156101290+AtanasBondzhakov@users.noreply.github.com>
2024-07-04 20:50:07 +0300

    Update and rename 2b. Data Types and Variables/03-townInfo.js to 2. Data Types and Variables/Exercises/03-townInfo.js

--- 2b. Data Types and Variables/03-townInfo.js
+++
@@ -1,6 +0,0 @@
-function townInfo(town, population, area) {
-    console.log(`Town ${town} has population of ${population} and area ${area} square km.`);
-}
-
-townInfo('Sofia', 1286383, 492); //Town Sofia has population of 1286383 and area 492 square km.
-townInfo('Plovdiv', 1481353, 512); //Town Plovdiv has population of 1481353 and area 512 square km.
