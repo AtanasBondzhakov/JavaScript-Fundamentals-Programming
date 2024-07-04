615bbfb93586a8d52dab4f8fc601bec7218d912a
Atanas Bondzhakov <156101290+AtanasBondzhakov@users.noreply.github.com>
2024-07-04 20:46:56 +0300

    Update and rename 2a. Data Types and Variables/06-gramophone.js to 2. Data Types and Variables/Lab/06-gramophone.js

--- 2. Data Types and Variables/Lab/06-gramophone.js
+++ 2. Data Types and Variables/Lab/06-gramophone.js
@@ -3,6 +3,3 @@ function gramophone(band, album, song) {
     let rotationCount = Math.ceil(songDurationInSec / 2.5);
     console.log(`The plate was rotated ${rotationCount} times.`);
 }
-
-gramophone('Black Sabbath', 'Paranoid', 'War Pigs'); //The plate was rotated 167 times.
-gramophone('Rammstein', 'Sehnsucht', 'Engel'); //The plate was rotated 81 times.
