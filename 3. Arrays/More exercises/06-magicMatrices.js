40ffaba0567280f05cc7d40901bd441dd0c45818
Atanas Bondzhakov <156101290+AtanasBondzhakov@users.noreply.github.com>
2024-07-05 23:28:46 +0300

    Update and rename 3c. Arrays/06-magicMatrices.js to 3. Arrays/More exercises/06-magicMatrices.js

--- 3. Arrays/More exercises/06-magicMatrices.js
+++ 3. Arrays/More exercises/06-magicMatrices.js
@@ -19,6 +19,3 @@ function magicMatrices(matrix) {
     }
     isMagic === true ? console.log(true) : console.log(false);
 }
-
-magicMatrices([[4, 5, 6], [6, 5, 4], [5, 5, 5]]); //true
-magicMatrices([[11, 32, 45], [21, 0, 1], [21, 1, 1]]); //false
