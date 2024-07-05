c6e2f2b171c2fc695b1c59cbac45691e4d24a4c3
Atanas Bondzhakov <156101290+AtanasBondzhakov@users.noreply.github.com>
2024-07-05 23:27:18 +0300

    Update and rename 3c. Arrays/03-rotateArray.js to 3. Arrays/More exercises/03-rotateArray.js

--- 3. Arrays/More exercises/03-rotateArray.js
+++ 3. Arrays/More exercises/03-rotateArray.js
@@ -9,6 +9,3 @@ function rotateArray(data) {
 
     console.log(result.join(' '));
 }
-
-rotateArray(['1', '2', '3', '4', '2']); //3 4 1 2
-rotateArray(['Banana', 'Orange', 'Coconut', 'Apple', '15']); //Orange Coconut Apple Banana
