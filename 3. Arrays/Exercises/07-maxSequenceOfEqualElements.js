7a6673cae7b9e76e1b070cdd451715ff29e676c0
Atanas Bondzhakov <156101290+AtanasBondzhakov@users.noreply.github.com>
2024-07-05 11:30:14 +0300

    Update and rename 3b. Arrays/07-maxSequenceOfEqualElements.js to 3. Arrays/Exercises/07-maxSequenceOfEqualElements.js

--- 3. Arrays/Exercises/07-maxSequenceOfEqualElements.js
+++ 3. Arrays/Exercises/07-maxSequenceOfEqualElements.js
@@ -19,8 +19,3 @@ function maxSequenceOfEqualElements(arr) {
 
     console.log(maxSequence.join(' '));
 }
-
-maxSequenceOfEqualElements([2, 1, 1, 2, 3, 3, 2, 2, 2, 1]); //2 2 2
-maxSequenceOfEqualElements([1, 1, 1, 2, 3, 1, 3, 3]); //1 1 1
-maxSequenceOfEqualElements([4, 4, 4, 4]); //4 4 4 4
-maxSequenceOfEqualElements([0, 1, 1, 5, 2, 2, 6, 3, 3]); //1 1
