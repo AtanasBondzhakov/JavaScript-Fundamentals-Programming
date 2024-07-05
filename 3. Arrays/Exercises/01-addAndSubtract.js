f6285c85e336eb7ea199e1c62e44b1d4e7d4bdc4
Atanas Bondzhakov <156101290+AtanasBondzhakov@users.noreply.github.com>
2024-07-05 11:28:02 +0300

    Update and rename 3b. Arrays/01-addAndSubtract.js to 3. Arrays/Exercises/01-addAndSubtract.js

--- 3. Arrays/Exercises/01-addAndSubtract.js
+++ 3. Arrays/Exercises/01-addAndSubtract.js
@@ -19,6 +19,3 @@ function addAndSubtract(arr) {
     console.log(sumInitialArr);
     console.log(sumResultArr);
 }
-
-addAndSubtract([5, 15, 23, 56, 35]); //[ 5, 14, 21, 59, 31 ], 134, 130
-addAndSubtract([-5, 11, 3, 0, 2]); //[ -5, 10, 1, 3, 6 ], 11, 15
