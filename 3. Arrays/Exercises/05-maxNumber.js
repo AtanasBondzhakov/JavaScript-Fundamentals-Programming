d59fe646c2353d34ae41a28b90a813bd97e3cd10
Atanas Bondzhakov <156101290+AtanasBondzhakov@users.noreply.github.com>
2024-07-05 11:29:34 +0300

    Update and rename 3b. Arrays/05-maxNumber.js to 3. Arrays/Exercises/05-maxNumber.js

--- 3. Arrays/Exercises/05-maxNumber.js
+++ 3. Arrays/Exercises/05-maxNumber.js
@@ -20,7 +20,3 @@ function maxNumber(arr) {
 
     console.log(result.join(' '));
 }
-
-maxNumber([1, 4, 3, 2]); //4 3 2
-maxNumber([14, 24, 3, 19, 15, 17]); //24 19 17
-maxNumber([27, 19, 42, 2, 13, 45, 48]); //48
