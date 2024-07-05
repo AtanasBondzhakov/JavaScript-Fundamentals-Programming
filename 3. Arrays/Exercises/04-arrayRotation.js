ab87b0836edb0ac5ba6eded6904bb72d0544376b
Atanas Bondzhakov <156101290+AtanasBondzhakov@users.noreply.github.com>
2024-07-05 11:29:13 +0300

    Update and rename 3b. Arrays/04-arrayRotation.js to 3. Arrays/Exercises/04-arrayRotation.js

--- 3. Arrays/Exercises/04-arrayRotation.js
+++ 3. Arrays/Exercises/04-arrayRotation.js
@@ -6,7 +6,3 @@ function arrayRotation(arr, rotationsCount) {
 
     console.log(arr.join(' '));
 }
-
-arrayRotation([51, 47, 32, 61, 21], 2); //32 61 21 51 47
-arrayRotation([32, 21, 61, 1], 4); //32 21 61 1
-arrayRotation([2, 4, 15, 31], 5); //4 15 31 2
