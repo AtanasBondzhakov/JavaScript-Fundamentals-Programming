7537723d61846a70d6ec143c27ef9fe6d46d2a06
Atanas Bondzhakov <156101290+AtanasBondzhakov@users.noreply.github.com>
2024-07-05 23:29:12 +0300

    Update and rename 3c. Arrays/07-spiralMatrix.js to 3. Arrays/More exercises/07-spiralMatrix.js

--- 3. Arrays/More exercises/07-spiralMatrix.js
+++ 3. Arrays/More exercises/07-spiralMatrix.js
@@ -39,10 +39,3 @@ function spiralMatrix(rows, cols) {
 
     result = result.forEach(row => console.log(row.join(' ')));
 }
-
-spiralMatrix(5, 5); //1 2 3 4 5
-                    //16 17 18 19 6
-                    //15 24 25 20 7
-                    //14 23 22 21 8
-                    //13 12 11 10 9
-
