611744e0d916377dc74eb017890cef738663f133
Atanas Bondzhakov <156101290+AtanasBondzhakov@users.noreply.github.com>
2024-07-05 09:20:32 +0300

    Update and rename 3a. Arrays/03-reverseAnArrayOfNumbers.js to 3. Arrays/Lab/03-reverseAnArrayOfNumbers.js

--- 3. Arrays/Lab/03-reverseAnArrayOfNumbers.js
+++ 3. Arrays/Lab/03-reverseAnArrayOfNumbers.js
@@ -6,7 +6,3 @@ function reverseAnArrayOfNumbers(n, arr) {
     }
     console.log(result.join(' '));
 }
-
-reverseAnArrayOfNumbers(3, [10, 20, 30, 40, 50]); //30 20 10
-reverseAnArrayOfNumbers(4, [-1, 20, 99, 5]); //5 99 20 -1
-reverseAnArrayOfNumbers(2, [66, 43, 75, 89, 47]); //43 66
