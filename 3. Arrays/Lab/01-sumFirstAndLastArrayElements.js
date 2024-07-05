92a62b2cefa5e3a71f45db22ae5f66ce1f638777
Atanas Bondzhakov <156101290+AtanasBondzhakov@users.noreply.github.com>
2024-07-05 09:19:51 +0300

    Update and rename 3a. Arrays/01-sumFirstAndLastArrayElements.js to 3. Arrays/Lab/01-sumFirstAndLastArrayElements.js

--- 3. Arrays/Lab/01-sumFirstAndLastArrayElements.js
+++ 3. Arrays/Lab/01-sumFirstAndLastArrayElements.js
@@ -4,7 +4,3 @@ function sumFirstAndLastArrayElements(numbers) {
     const sum = firstNum + lastNum;
     console.log(sum);
 }
-
-sumFirstAndLastArrayElements([20, 30, 40]); //60
-sumFirstAndLastArrayElements([10, 17, 22, 33]); //43
-sumFirstAndLastArrayElements([11, 58, 69]); //80
