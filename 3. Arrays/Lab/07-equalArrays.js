5eda58bacf21cf81a716bd6bd76b1038768cf0a8
Atanas Bondzhakov <156101290+AtanasBondzhakov@users.noreply.github.com>
2024-07-05 09:21:49 +0300

    Update and rename 3a. Arrays/07-equalArrays.js to 3. Arrays/Lab/07-equalArrays.js

--- 3. Arrays/Lab/07-equalArrays.js
+++ 3. Arrays/Lab/07-equalArrays.js
@@ -20,7 +20,3 @@ function equalArrays(arr1, arr2) {
         console.log(`Arrays are not identical. Found difference at ${indexDifference} index`);
     }
 }
-
-equalArrays(['10', '20', '30'], ['10', '20', '30']); //Arrays are identical. Sum: 60
-equalArrays(['1', '2', '3', '4', '5'], ['1', '2', '4', '4', '5']); //Arrays are not identical. Found difference at 2 index
-equalArrays(['1'], ['10']); //Arrays are not identical. Found difference at 0 index
