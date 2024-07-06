72a12a1592bb3621fb934e8562fee195c86e6d51
Atanas Bondzhakov <156101290+AtanasBondzhakov@users.noreply.github.com>
2024-07-06 23:15:05 +0300

    Update and rename 4a. Functions/01-formatGrade.js to 4. Functions/Lab/01-formatGrade.js

--- 4. Functions/Lab/01-formatGrade.js
+++ 4. Functions/Lab/01-formatGrade.js
@@ -19,7 +19,3 @@ function formatGrade(grade) {
         console.log(`${result} (${grade.toFixed(2)})`);
     }
 }
-
-formatGrade(3.33); //Poor (3.33)
-formatGrade(4.50); //Very good (4.50)
-formatGrade(2.99); //Fail (2)
