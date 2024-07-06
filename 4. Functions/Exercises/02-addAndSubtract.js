efcf62d3f8e7b249f35283dd2f94b19af179f0d6
Atanas Bondzhakov <156101290+AtanasBondzhakov@users.noreply.github.com>
2024-07-06 23:21:59 +0300

    Update and rename 4b. Functions/02-addAndSubtract.js to 4. Functions/Exercises/02-addAndSubtract.js

--- 4. Functions/Exercises/02-addAndSubtract.js
+++ 4. Functions/Exercises/02-addAndSubtract.js
@@ -11,7 +11,3 @@ function addAndSubtract(num1, num2, num3) {
     const result = subtract(medialResult, num3);
     return result;
 }
-
-addAndSubtract(23, 6, 10); //19
-addAndSubtract(1, 17, 30); //-12
-addAndSubtract(42, 58, 100); //0
