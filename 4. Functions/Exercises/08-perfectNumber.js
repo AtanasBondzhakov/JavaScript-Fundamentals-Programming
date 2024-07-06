fd968afb83f6f10c8086eacde242c057c3e27403
Atanas Bondzhakov <156101290+AtanasBondzhakov@users.noreply.github.com>
2024-07-06 23:24:27 +0300

    Update and rename 4b. Functions/08-perfectNumber.js to 4. Functions/Exercises/08-perfectNumber.js

--- 4. Functions/Exercises/08-perfectNumber.js
+++ 4. Functions/Exercises/08-perfectNumber.js
@@ -12,7 +12,3 @@ function perfectNumber(num) {
         console.log('It\'s not so perfect.');
     }
 }
-
-perfectNumber(6); //We have a perfect number!
-perfectNumber(28); //We have a perfect number!
-perfectNumber(1236498); //It's not so perfect.
