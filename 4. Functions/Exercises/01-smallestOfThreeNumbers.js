beaa2ab887e3c4487caa014d85cabc23d870c952
Atanas Bondzhakov <156101290+AtanasBondzhakov@users.noreply.github.com>
2024-07-06 23:21:32 +0300

    Update and rename 4b. Functions/01-smallestOfThreeNumbers.js to 4. Functions/Exercises/01-smallestOfThreeNumbers.js

--- 4. Functions/Exercises/01-smallestOfThreeNumbers.js
+++ 4. Functions/Exercises/01-smallestOfThreeNumbers.js
@@ -7,8 +7,3 @@ function smallestOfThreeNumbers(num1, num2, num3) {
     const smaller = smallerNumber(num1, num2);
     return smallerNumber(smaller, num3);
 }
-
-smallestOfThreeNumbers(2, 5, 3); //2
-smallestOfThreeNumbers(25, 21, 4); //123
-smallestOfThreeNumbers(600, 342, 123); //4
-smallestOfThreeNumbers(2, 2, 2); //2
