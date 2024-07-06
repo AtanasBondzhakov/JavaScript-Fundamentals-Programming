7bcfef3a972d820ff959f96a6993cc69e19a4ff0
Atanas Bondzhakov <156101290+AtanasBondzhakov@users.noreply.github.com>
2024-07-06 23:29:44 +0300

    Update and rename 4c. Functions/03-pointsValidation.js to 4. Functions/More exercises/03-pointsValidation.js

--- 4. Functions/More exercises/03-pointsValidation.js
+++ 4. Functions/More exercises/03-pointsValidation.js
@@ -9,10 +9,3 @@ function pointsValidation([x1, y1, x2, y2]) {
     console.log(`{${x2}, ${y2}} to {0, 0} is ${isDistanceInt(x2, y2, 0, 0)} `);
     console.log(`{${x1}, ${y1}} to {${x2}, ${y2}} is ${isDistanceInt(x1, y1, x2, y2)} `);
 }
-
-pointsValidation([3, 0, 0, 4]); //{3, 0} to {0, 0} is valid
-                                //{0, 4} to {0, 0} is valid
-                                //{3, 0} to {0, 4} is valid
-pointsValidation([2, 1, 1, 1]); //{2, 1} to {0, 0} is invalid
-                                //{1, 1} to {0, 0} is invalid
-                                //{2, 1} to {1, 1} is valid
