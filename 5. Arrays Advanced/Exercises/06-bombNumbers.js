bba1fbd53b61ab480fd0beefb224f23ac00b08f8
Atanas Bondzhakov <156101290+AtanasBondzhakov@users.noreply.github.com>
2024-07-08 23:34:23 +0300

    Update and rename 5b. Arrays Advanced/06-bombNumbers.js to 5. Arrays Advanced/Exercises/06-bombNumbers.js

--- 5. Arrays Advanced/Exercises/06-bombNumbers.js
+++ 5. Arrays Advanced/Exercises/06-bombNumbers.js
@@ -14,7 +14,3 @@ function bombNumbers(sequence, bomb) {
     sum = sequence.reduce((acc, val) => acc + val, 0);
     console.log(sum);
 }
-
-bombNumbers([1, 2, 2, 4, 2, 2, 2, 9], [4, 2]); //12
-bombNumbers([1, 4, 4, 2, 8, 9, 1], [9, 3]); //5
-bombNumbers([1, 7, 7, 1, 2, 3], [7, 1]); //6
