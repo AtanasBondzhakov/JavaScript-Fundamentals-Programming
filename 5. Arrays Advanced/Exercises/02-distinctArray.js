dbc61ef3c29f26ecda7b0d8ae2292321df9c3bb5
Atanas Bondzhakov <156101290+AtanasBondzhakov@users.noreply.github.com>
2024-07-08 23:32:49 +0300

    Update and rename 5b. Arrays Advanced/02-distinctArray.js to 5. Arrays Advanced/Exercises/02-distinctArray.js

--- 5. Arrays Advanced/Exercises/02-distinctArray.js
+++ 5. Arrays Advanced/Exercises/02-distinctArray.js
@@ -9,7 +9,3 @@ function distinctArray(data) {
 
     console.log(result.join(' '));
 }
-
-distinctArray([1, 2, 3, 4]); //1 2 3 4
-distinctArray([7, 8, 9, 7, 2, 3, 4, 1, 2]); //7 8 9 2 3 4 1
-distinctArray([20, 8, 12, 13, 4, 4, 8, 5]); //20 8 12 13 4 5
