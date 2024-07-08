4c269cd6b177abf365409c573c5e93c715e12634
Atanas Bondzhakov <156101290+AtanasBondzhakov@users.noreply.github.com>
2024-07-08 23:35:07 +0300

    Update and rename 5b. Arrays Advanced/08-arrayManipulator.js to 5. Arrays Advanced/Exercieses/08-arrayManipulator.js

--- 5. Arrays Advanced/Exercieses/08-arrayManipulator.js
+++ 5. Arrays Advanced/Exercieses/08-arrayManipulator.js
@@ -47,7 +47,3 @@ function arrayManipulator(integers, commands) {
         }
     }
 }
-
-arrayManipulator([1, 2, 3, 4, 5],
-    ['addMany 5 9 8 7 6 5', 'contains 15', 'remove 3', 'shift 1', 'sumPairs', 'print']
-);
