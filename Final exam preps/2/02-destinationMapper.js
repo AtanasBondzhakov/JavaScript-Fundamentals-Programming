8fb7834cbe25484e9c1c89299a9e8bbb736364e6
Atanas Bondzhakov <atanas.b@yahoo.com>
2024-07-19 21:01:12 +0300

    Create 02-destinationMapper.js

---
+++ Final exam preps/2/02-destinationMapper.js
@@ -0,0 +1,18 @@
+function destinationMapper(text) {
+    const pattern = /([=\/])[A-Z][A-Za-z]{2,}\1/g;
+    let matches = pattern.exec(text);
+    let destinations = [];
+
+    let travelPoints = 0;
+
+    while (matches !== null) {
+        destinations.push(matches[0]);
+        matches = pattern.exec(text);
+    }
+
+    destinations = destinations.map(d => d.slice(1, d.length - 1));
+    destinations.forEach(d => travelPoints += d.length);
+
+    console.log(`Destinations: ${destinations.join(', ')}`);
+    console.log(`Travel Points: ${travelPoints}`);
+}
